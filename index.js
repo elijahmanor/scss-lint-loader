4515ed89b1d089806f8aa706e5e44ea15a8f1f52
Elijah Manor <elijah.manor@gmail.com>
2015-07-29 14:16:34 -0500

    Basic functionality

---
+++ index.js
@@ -0,0 +1,46 @@
+// Dependencies
+
+var SCSSLint = require('broccoli-scss-lint/lib/scss-lint');
+var loaderUtils = require('loader-utils');
+var assign = require('object-assign');
+
+function lint(input, options, webpack) {
+  options.format = SCSSLint.format(options.format);
+  SCSSLint.validate(options);
+  var success = SCSSLint.lint('', webpack.resourcePath, options);
+
+  if (!success) {
+    webpack.emitError('Module failed because of a scss-lint error!');
+    /*
+    if (options.failOnError) {
+      throw new Error('Module failed because of a scss-lint error.');
+    }
+    } else if (output.warnings) {
+      webpack.emitWarning(output.message);
+    }
+    */
+  }
+}
+
+/**
+ * Webpack Loader
+ *
+ * @param {String|Buffer} input JavaScript string
+ * @returns {String|Buffer} original input
+ */
+module.exports = function(input) {
+  var options = assign(
+    {
+      configFile: './.scss-lint.yml',
+      failOnError: true,
+      failWarning: true,
+      format: ['default', 'xml'], //'default',
+      reportFile: './scss-lint.xml'
+    },
+    loaderUtils.parseQuery(this.query)
+  );
+
+  lint(input, options, this);
+
+  return input;
+};
